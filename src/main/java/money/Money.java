f838b06c99d754f2a999fcfdf850d9ddd6ce1314
jeongseongwon99 <sw990320@naver.com>
2026-02-18 22:59:33 +0900

    fix: git 병합 에러 해결

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -55,6 +55,10 @@ public final class Money {
         return value;
     }
 
+    public long value() {
+        return value;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (obj == this) return true;
